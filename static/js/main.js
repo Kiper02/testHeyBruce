066e87e771fb901674f28386b8705f87e2a819dc
Кирилл Пермяков <149666602+Kiper02@users.noreply.github.com>
2024-03-08 14:09:39 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 5000,
+      },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.btn-next',
+      prevEl: '.btn-prev',
+    },
+
+  });
